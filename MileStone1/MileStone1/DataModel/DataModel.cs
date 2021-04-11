143f68252f99fe756fe6b2a137a5818cf3c9b9f6
TalBenZvi <talbz03@gmail.com>
2021-04-11 23:45:24 +0300

    updated file read model interface

--- MileStone1/MileStone1/DataModel/DataModel.cs
+++ MileStone1/MileStone1/DataModel/DataModel.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace MileStone1.dataModel {
     class DataModel : IDataModel {
-        private const int DEFAULT_INTERVAL_IN_MILLIS = 100;
+        private const int MILLISECONDS_PER_DATA_LINE = 100;
+        private const double MILLISECONDS_IN_A_SECOND = 1000;
+        private const double DATA_LINES_PER_SECOND = MILLISECONDS_IN_A_SECOND / MILLISECONDS_PER_DATA_LINE;
 
         private ITelnetClient telnetClient;
         private double[][] data;
+
+        private bool pause, stop;
+
         public delegate void UsePropertyUpdate(string propety, double newValue);
         public event UsePropertyUpdate PropertyUpdated;
 
-        private double time;
+        private int rowIndex;
         public double Time {
             get {
-                return time;
+                return rowIndex / DATA_LINES_PER_SECOND;
             }
             set {
-                time = value;
+                rowIndex = (int)(value * DATA_LINES_PER_SECOND);
             }
         }
 
@@ -36,6 +42,8 @@ namespace MileStone1.dataModel {
             this.data = data;
             Time = 0;
             Speed = 1;
+            stop = false;
+            pause = false;
         }
 
         public void Connect(string ip, int port) {
@@ -47,7 +55,11 @@ namespace MileStone1.dataModel {
         }
 
         public void Start() {
+            new Thread(delegate () {
+                while (!stop && !pause && rowIndex < data.Length) {
 
+                }
+            }).Start();
         }
     }
 }
--- MileStone1/MileStone1/FileRead/IFileReadModel.cs
+++ MileStone1/MileStone1/FileRead/IFileReadModel.cs
@@ -4,11 +4,19 @@ using System.Text;
 
 namespace MileStone1 {
     interface IFileReadModel {
-        public void ReadFile(string FilePath);
+        public void ReadDataFile(string FilePath);
 
         public double[][] GetDataLog();
 
-        public delegate void UseLog();
-        public event UseLog ReadFinished;
+        public delegate void UseDataLog();
+        public event UseDataLog DataReadFinished;
+
+        public void ReadDefinitionFile(string FilePath);
+
+        public double GetSampleRate();
+        public double[] GetDefinitions();
+
+        public delegate void UseDefinitions();
+        public event UseDefinitions DefinitionReadFinished;
     }
 }
