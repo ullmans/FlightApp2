c41ddc8288635b7c8eaa54a9f579cf0bf5c0f394
TalBenZvi <talbz03@gmail.com>
2021-04-13 15:40:52 +0300

    added file read view model

--- MileStone1/MileStone1/FileRead/FileReadViewModel.cs
+++ MileStone1/MileStone1/FileRead/FileReadViewModel.cs
@@ -1,35 +1,64 @@
 using System.ComponentModel;
 using Microsoft.Win32;
+using System.Collections.Generic;
 
 namespace MileStone1 {
-    class FileReadViewModel : IPlaneViewModel {
-        private IDataModel model;
+    class FileReadViewModel : IFileReadViewModel {
+        private IFileReadModel model;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public FileReadViewModel(IFileReadModel model) {
+            this.model = model;
+            this.model.FileReadFinished += useResult;
+        }
 
-        private string filePath;
-        public string VM_FilePath {
-            get { return filePath; }
-            set 
-            {
-                filePath = value;
-                NotifyPropertyChanged("VM_FilePath");
+        public event IFileReadViewModel.UseResult FileReadFinished;
+
+        private string dataFilePath;
+        public string DataFilePath {
+            get {
+                return dataFilePath;
+            }
+            set {
+                dataFilePath = value;
             }
         }
 
-        public FileReadViewModel(IDataModel model) {
-            filePath = "";
-            this.model = model;
+        private string definitionsFilePath;
+        public string DefinitionsFilePath {
+            get {
+                return definitionsFilePath;
+            }
+            set {
+                definitionsFilePath = value;
+            }
         }
 
-        public void StartSimulation() {
-            model.Connect("localhost", 8081);
-            model.Start(filePath);  
+        public void ReadFile(FileType fileType) {
+            string filePath = null;
+            if (fileType == FileType.Data) {
+                filePath = DataFilePath;
+            } else if (fileType == FileType.Definitions) {
+                filePath = DefinitionsFilePath;
+            }
+            model.ReadFile(filePath, fileType);
         }
 
-        public void NotifyPropertyChanged(string propName) {
-            if (this.PropertyChanged != null)
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        public List<double[]> GetDataLog() {
+            return model.GetDataLog();
+        }
+
+        public double GetSampleRate() {
+            return model.GetSampleRate();
+        }
+
+        public List<string> GetDefinitions() {
+            return model.GetDefinitions();
+        }
+
+        public void useResult(object sender, FileType fileType) {
+            if (sender as IFileReadModel == model && FileReadFinished != null) {
+                FileReadFinished(this, fileType);
+            }
         }
     }
 }
--- MileStone1/MileStone1/IPlaneViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel;
-namespace MileStone1 {
-    interface IPlaneViewModel : INotifyPropertyChanged { 
-        void StartSimulation();
-
-        string VM_FilePath { set; get; }
-    }
-}
