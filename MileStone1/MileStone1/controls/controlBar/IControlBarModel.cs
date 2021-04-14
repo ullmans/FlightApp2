a8e6678e5b3944cd158f8ac30f80d229cd8e9603
רן תמ <4roytamir@gmail.com>
2021-04-14 22:10:55 +0300

    fixed stupid mistake in last commit

--- MileStone1/MileStone1/controls/controlBar/IControlBarModel.cs
+++ MileStone1/MileStone1/controls/controlBar/IControlBarModel.cs
@@ -7,11 +7,11 @@ public interface IControlBarModel : INotifyPropertyChanged
     // says whether or not the plane display should be running
     bool Running { get; set; }
     // number of lines in a flight file
-    int Lines { get; set; }
+    int Lines { get; }
     // line in flight file the flight display is showing
     int Position { get; set; }
     // the speed at which the flight display is running
-    double PlaySpeed { get; }
+    double PlaySpeed { get; set; }
 
     // updates control bar after moving to next line in flight file
     void Move();
